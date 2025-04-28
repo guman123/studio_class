f139f5ff10bc5b4f99b5167f361c4ce6b48295ad
guman123 <rbals1593@naver.com>
2025-04-28 13:47:56 +0900

    base version

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,13 @@
 import streamlit as st
 
+st.set_page_config(page_title="OCR 프로젝트", layout="wide") #centered
+st.header('판서 이미지 선택')
+
+uploaded_file = st.file_uploader("변환할 이미지를 선택하세요", type=['png','jpg'])
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption='글씨 이미지', width=400)
+
+st.button("텍스트 변환")
+
+
